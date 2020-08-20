44deb6e3f9e66f22c3f4b95d903aa37402512d1e
bluebrown <39703898+bluebrown@users.noreply.github.com>
2020-08-20 16:01:49 +0200

    add animation

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Tailwindcss from "./Tailwindcss.svelte";
+	import { fly } from "svelte/transition";
 	export let name;
 	let fruits;
 	let dispense = fruit => (fruits = fruits.filter(f => f !== fruit));
@@ -7,7 +8,7 @@
 	refill();
 </script>
 
-<main class="text-center p-5">
+<main class="text-center p-5 overflow-hidden h-full">
 		<h1 class="uppercase text-6xl font-hairline text-svelte mb-3">Hello, {name}!</h1>
 	<p class="mb-1">
 		Visit the
@@ -23,17 +24,22 @@
 
 <hr class="my-3">
 <h2 class="font-semibold mb-2">Pick a Fruit</h2>
-<ul class="flex justify-center -mx-1">
 	{#if fruits.length < 1} 
-		<li class="px-1"><button on:click={refill}
-			class="px-3 py-1 border focus:outline-none focus:shadow-outline bg-blue-600 hover:bg-blue-500 text-white rounded-md shadow-md"
-		>Refill</button></li> 
+			<button 
+				on:click={refill}
+				in:fly="{{ x: -300, duration: 700, delay: 250}}" 
+				class="px-3 py-1 border focus:outline-none focus:shadow-outline bg-blue-600 hover:bg-blue-500 text-white rounded-md shadow-md"
+		>Refill</button>
 	{/if}
-	{#each fruits as fruit}
-		<li  on:click={dispense(fruit)}
-				class="px-1"
+<ul class="flex justify-center -mx-1">
+	{#each fruits as fruit, index (fruit)}
+		<li  
+			on:click={dispense(fruit)}
+			in:fly="{{ y: 300, duration: 3000 }}"  out:fly="{{ y: -120, duration: 700 }}"
+			class="px-1"
 		>
-			<button class=" cursor-pointer border p-3 bg-orange-400 hover:bg-orange-300 shadow-md focus:outline-none focus:shadow-outline">
+			<button
+			 class="rounded-md cursor-pointer border p-3 bg-orange-400 hover:bg-orange-300 shadow-md focus:outline-none focus:shadow-outline">
 				{fruit}
 			</button>
 		</li>
